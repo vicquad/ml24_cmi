dbcf339a88cb297cb3a78e8a31d5f99db15d6c5e
Hanna Junk <hanna.junk@ut.ee>
2024-11-22 23:33:17 +0200

    add and test methods for data imputation and oversampling

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from imblearn.over_sampling import SMOTENC
+from sklearn.impute import KNNImputer, SimpleImputer
 from sklearn.metrics import cohen_kappa_score
 import os
 
@@ -112,3 +114,99 @@ def get_labeled_subset(data):
     """
     data = data.dropna(subset=["sii"])
     return data 
+
+
+def impute_tabdata(data, dict_path):
+    """
+    Imputes missing values in the given set of tabular data.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Input data, including target column "sii". Can be train or test set.
+    
+    dict_path: str
+        Path to data dictionary csv file.
+
+    Returns
+    -------
+    X : DataFrame
+        Imputed input data.
+
+    """
+    data_dict = pd.read_csv(dict_path)
+
+    # Separate target value
+    target = data["sii"]
+    data = data.drop(columns="sii")
+
+    # Identify categorical integer fields
+    categorical_int_features = data_dict[data_dict['Type'].str.contains('categorical int', na=False)]['Field'].tolist()
+    
+    # Drop PCIAT features as they are not part of train data
+    categorical_int_features = [s for s in categorical_int_features if "PCIAT" not in s]
+
+    # Extract numerical features
+    numerical_features = data_dict[
+        data_dict['Type'].str.contains('float|int', na=False) & 
+        ~data_dict['Type'].str.contains('categorical|str', na=False)
+    ]['Field'].tolist()
+
+    # Drop PCIAT features as they are not part of train data
+    numerical_features = [s for s in numerical_features if "PCIAT" not in s]
+
+    # Extract categorical features
+    categorical_features = data_dict[
+        data_dict["Type"].str.contains("categorical|str", na=False)
+    ]["Field"].tolist()
+    
+    # Drop PCIAT features as they are not part of train data
+    categorical_features = [s for s in categorical_features if "PCIAT" not in s]
+
+    # ensure categorical features are of type str
+    data[categorical_features] = data[categorical_features].astype(str)
+
+    # impute numerical features with KNNImputer
+    num_imputer = KNNImputer(n_neighbors=3)
+    data[numerical_features] = num_imputer.fit_transform(data[numerical_features])
+
+    # impute numerical features with SimpleImputer - most frequent value
+    cat_imputer = SimpleImputer(strategy="most_frequent")
+    imputed_categorical = cat_imputer.fit_transform(data[categorical_features])
+    
+    # Convert the result back to a DataFrame with the original column names
+    data[categorical_features] = pd.DataFrame(imputed_categorical, columns=categorical_features, index=data.index)
+    
+    # Combine data and target again
+    data["sii"] = target
+
+    return data
+
+def oversample_tabdata(data):
+    """
+    Oversamples minority classes for given tabular data.
+ 
+    Parameters
+    ----------
+    data : DataFrame
+        Input data, including target column "sii". 
+        This should be the label-propagated train set with no missing labels.
+
+    Returns
+    -------
+    X_resampled : DataFrame
+        Oversampled input data.
+
+    """
+
+    y = data["sii"]
+    X = data.drop(columns="sii")
+
+    categorical_features = data.select_dtypes(include=['object', 'category']).columns.to_list()
+
+    smote_nc = SMOTENC(categorical_features=categorical_features, random_state=42)
+    X_resampled, y_resampled = smote_nc.fit_resample(X, y)
+
+    X_resampled["sii"] = y_resampled
+
+    return X_resampled
